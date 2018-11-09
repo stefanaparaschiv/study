e2762474d95d3a7cfac9ce01e6a730d44707f95b
Stefana PARASCHIV <sparaschiv@pentalog.com>
2018-11-09 16:21:13 +0200

    fixed query

--- managementApp/src/main/java/managementapp/repository/MenuRepository.java
+++ managementApp/src/main/java/managementapp/repository/MenuRepository.java
@@ -14,7 +14,7 @@ public interface MenuRepository extends CrudRepository<Menu, Long> {
 	public List<Menu> findByName(String name);
 	
 	@Query(value = "SELECT * FROM Menu m WHERE m.price <= :priceLimit",nativeQuery = true)
-	public List<Menu> findByPriceBelow(int i);
+	public List<Menu> findByPriceBelow(int priceLimit);
 	
 	@Query(value = "SELECT * FROM Menu m WHERE UPPER(m.type) = :type",nativeQuery = true)
 	public List<Menu> findByType(String type);	
